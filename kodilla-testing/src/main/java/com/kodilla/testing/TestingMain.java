50867fac1982bbb23eb630b4322b35d23140390e
JMagn <40744844+JMagn@users.noreply.github.com>
2018-07-19 00:22:12 +0200

    Task 2 from module 6 (#2)

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,24 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        Calculator calculator = new Calculator();
+
+        int resultAdd = calculator.add(5, 10);
+        int resultSub = calculator.substract(5, 10);
+
+        if (resultAdd == 15) {
+            System.out.println("Test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        if (resultSub == -5) {
+            System.out.println("Test OK");
+        } else {
+            System.out.println("Error!");
+        }
     }
 }
