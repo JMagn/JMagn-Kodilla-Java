78c6624c234f14bcf902337536a37651b1f055ad
JMagn <jakub.magnuski@gmail.com>
2018-07-31 13:01:48 +0200

    Task 1 from module 7

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,7 +1,14 @@
 package com.kodilla.stream;
 
+import com.kodilla.stream.beautifier.PoemBeautifier;
+
 public class StreamMain {
     public static void main(String[] args) {
-        System.out.println("Welcome to module 7 - Stream");
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+
+        poemBeautifier.beautify("Litwo, ojczyzno moja", (string -> "***" + string + "***"));
+        poemBeautifier.beautify("Ty jesteś jak zdrowie", (string -> string.toUpperCase()));
+        poemBeautifier.beautify("Ile Cię trzeba cenić", (string -> "& Ile & Cię & trzeba & cenić &"));
+        poemBeautifier.beautify("Ten tylko się dowie", (string -> string.concat("!!!")));
     }
 }
