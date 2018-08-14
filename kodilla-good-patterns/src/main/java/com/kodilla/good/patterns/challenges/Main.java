af34a1d2ce46455e13470eb0a6d89a006cc37cb4
JMagn <jakub.magnuski@gmail.com>
2018-08-15 00:28:02 +0200

    Task from submodule 9.1.

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/Main.java
@@ -0,0 +1,17 @@
+package com.kodilla.good.patterns.challenges;
+
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        MovieStore movieStore = new MovieStore();
+
+        String result = movieStore.getMovies().values().stream()
+                .flatMap(l -> l.stream())
+                .collect(Collectors.joining(" ! ", "", ""));
+
+        System.out.println(result);
+    }
+}
