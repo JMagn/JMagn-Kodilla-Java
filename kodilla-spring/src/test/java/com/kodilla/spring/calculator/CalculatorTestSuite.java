b81ccfc40a128f74ffd07146883da8d429b3333e
JMagn <jakub.magnuski@gmail.com>
2018-08-24 01:51:37 +0200

    Task from submodule 10.2.

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -0,0 +1,27 @@
+package com.kodilla.spring.calculator;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CalculatorTestSuite {
+
+    @Test
+    public void testCalculations() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        //When
+        Calculator calculator = context.getBean(Calculator.class);
+        //Then
+        Assert.assertEquals(12, calculator.add(10, 2), 000.1);
+        Assert.assertEquals(8, calculator.sub(10, 2), 000.1);
+        Assert.assertEquals(20, calculator.mul(10, 2), 000.1);
+        Assert.assertEquals(5, calculator.div(10, 2), 000.1);
+    }
+}
