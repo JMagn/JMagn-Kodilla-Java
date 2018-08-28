9f6eeef2dc5366395b6c6aad4b91db33eff6734a
JMagn <jakub.magnuski@gmail.com>
2018-08-29 01:15:04 +0200

    Task from submodule 11.2.

---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/prototype/library/Library.java
@@ -0,0 +1,61 @@
+package com.kodilla.patterns.prototype.library;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public final class Library extends Prototype {
+    String name;
+    Set<Book> books = new HashSet<>();
+
+    public Library(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Library shallowCopy() throws CloneNotSupportedException {
+        return (Library)super.clone();
+    }
+
+    public Library deepCopy() throws CloneNotSupportedException {
+        Library cloneLibrary = (Library)super.clone();
+        cloneLibrary.books = new HashSet<>();
+        for (Book book : this.books) {
+            cloneLibrary.books.add(book);
+        }
+        return cloneLibrary;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Library library = (Library) o;
+        return Objects.equals(name, library.name) &&
+                Objects.equals(books, library.books);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, books);
+    }
+
+    @Override
+    public String toString() {
+        return "Library{" +
+                "name='" + name + '\'' +
+                ", books=" + books +
+                '}';
+    }
+}
