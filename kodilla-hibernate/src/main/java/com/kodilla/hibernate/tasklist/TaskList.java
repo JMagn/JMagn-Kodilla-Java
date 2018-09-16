4825bf7b279f466c06aa4f64f89f3b7ce4230995
JMagn <40744844+JMagn@users.noreply.github.com>
2018-09-17 01:05:06 +0200

    Example 13.3. to merge (#36)
    * Example 13.2.
    
    * Example update
    
    * Example 13.3.
    
    * Example update
    
    * Example update
    
    * Example update
    
    * Example 13.3. to merge

---
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/tasklist/TaskList.java
@@ -0,0 +1,92 @@
+package com.kodilla.hibernate.tasklist;
+import com.kodilla.hibernate.task.Task;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Table(name = "TASKLISTS")
+public class TaskList {
+    private int id;
+    private String listName;
+    private String description;
+    private List<Task> tasks = new ArrayList<>();
+
+    public TaskList() {
+    }
+
+    public TaskList(String listName, String description) {
+        this.listName = listName;
+        this.description = description;
+    }
+
+    @Id
+    @GeneratedValue
+    @NotNull
+    @Column(name = "ID", unique = true)
+    public int getId() {
+        return id;
+    }
+
+    @Column(name = "LISTNAME")
+    public String getListName() {
+        return listName;
+    }
+
+    @Column(name = "DESCRIPTION")
+    public String getDescription() {
+        return description;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setListName(String listName) {
+        this.listName = listName;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @OneToMany(
+            targetEntity = Task.class,
+            mappedBy = "taskList",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.EAGER
+    )
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TaskList taskList = (TaskList) o;
+        return id == taskList.id &&
+                Objects.equals(listName, taskList.listName) &&
+                Objects.equals(description, taskList.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, listName, description);
+    }
+
+    @Override
+    public String toString() {
+        return "TaskList{" +
+                "id=" + id +
+                ", listName='" + listName + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
