158bd9627d01a5d8dd03fb28ff0f1b90905cd9aa
JMagn <40744844+JMagn@users.noreply.github.com>
2018-11-09 14:04:45 +0100

    Task 23.3. (#50)
    * Task 23.3.
    
    * SelectByValue introduced

---
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
@@ -0,0 +1,33 @@
+package com.kodilla.testing2.facebook;
+
+import com.kodilla.testing2.config.WebDriverConfig;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+public class FacebookTestingApp {
+
+    public static final String XPATH_FILLDAY = "//div[contains(@class, \"_5k_5\")]/span/span/select[1]";
+    public static final String XPATH_FILLMONTH = "//div[contains(@class, \"_5k_5\")]/span/span/select[2]";
+    public static final String XPATH_FILLYEAR = "//div[contains(@class, \"_5k_5\")]/span/span/select[3]";
+
+    public static void main(String[] args) {
+        WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.FIREFOX);
+        driver.get("https://www.facebook.com/");
+
+        while (!driver.findElement(By.xpath(XPATH_FILLDAY)).isDisplayed());
+
+        WebElement selectComboDay = driver.findElement(By.xpath(XPATH_FILLDAY));
+        Select selectDay = new Select(selectComboDay);
+        selectDay.selectByIndex(12);
+
+        WebElement selectComboMonth = driver.findElement(By.xpath(XPATH_FILLMONTH));
+        Select selectMonth = new Select(selectComboMonth);
+        selectMonth.selectByIndex(4);
+
+        WebElement selectComboYear = driver.findElement(By.xpath(XPATH_FILLYEAR));
+        Select selectYear = new Select(selectComboYear);
+        selectYear.selectByValue("2000");
+    }
+}
