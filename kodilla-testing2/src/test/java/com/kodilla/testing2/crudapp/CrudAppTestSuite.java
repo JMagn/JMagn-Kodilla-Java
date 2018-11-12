7b4e775497208caebb3959f95b99737ef0541777
JMagn <40744844+JMagn@users.noreply.github.com>
2018-11-12 23:07:33 +0100

    Task 23.4. (#53)
    * Submodule update
    
    * Task 23.4.

--- kodilla-testing2/src/test/java/com/kodilla/testing2/crudapp/CrudAppTestSuite.java
+++ kodilla-testing2/src/test/java/com/kodilla/testing2/crudapp/CrudAppTestSuite.java
@@ -13,7 +13,6 @@ public class CrudAppTestSuite {
 
     private static final String BASE_URL = "https://jmagn.github.io/";
     private WebDriver driver;
-
     private Random generator;
 
     @Before
@@ -33,14 +32,32 @@ public class CrudAppTestSuite {
         final String XPATH_TASK_NAME = "//form[contains(@action,\"createTask\")]/fieldset[1]/input";
         final String XPATH_TASK_CONTENT = "//form[contains(@action,\"createTask\")]/fieldset[2]/textarea";
         final String XPATH_ADD_BUTTON = "//form[contains(@action,\"createTask\")]/fieldset[3]/button";
+
         String taskName = "Task number " + generator.nextInt(100000);
         String taskContent = taskName + " content";
+
         WebElement name = driver.findElement(By.xpath(XPATH_TASK_NAME));
         name.sendKeys(taskName);
+
         WebElement content = driver.findElement(By.xpath(XPATH_TASK_CONTENT));
         content.sendKeys(taskContent);
+
         WebElement addButton = driver.findElement(By.xpath(XPATH_ADD_BUTTON));
         addButton.click();
+
+        deleteCrudAppTestTask(taskName);
         Thread.sleep(2000);
+    }
+
+    private void deleteCrudAppTestTask(String taskName) throws InterruptedException {
+        Thread.sleep(5000);
+
+        driver.findElements(By.xpath("//form[@class=\"datatable__row\"]")).stream()
+                .filter(anyForm -> anyForm.findElement(By.xpath(".//p[@class=\"datatable__field-value\"]"))
+                        .getText().equals(taskName))
+                .forEach(theForm -> {
+                    WebElement deleteButton = theForm.findElement(By.xpath(".//button[4]"));
+                    deleteButton.click();
+                });
     }
 }
